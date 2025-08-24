a6a6d4803991cadf732a1b47073f09a901dcefab
arthurlegal552 <arthurcostagoncalves8@gmail.com>
2025-08-24 20:36:44 -0300

    game.js

--- game.js
+++ game.js
@@ -624,7 +624,7 @@ socket.on("remoteUnequip", (data) => {
     if (!remotePlayer) return;
 
     remotePlayer.userData.isUnequipping = true;
-    remotePlayer.userData.unequipAnimProgress = 0;
+    remotePlayer.userData.equipAnimProgress = 0;
 });
 
 
@@ -1588,7 +1588,6 @@ function unequipTool() {
     scene.add(rocketLauncherModel);
     rocketLauncherModel.visible = false;
     equippedTool = null;
-    isUnequipping = true;
     player.rightArm.rotation.x = 0; // Reset arm
     document.getElementById('equip-tool-btn').classList.remove('equipped');
 }
